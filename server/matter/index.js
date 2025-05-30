f19a1317292ce7e1b6b2c313f58c7f0ee9c90d4b
phothinmg <phothinmg@disroot.org>
2025-05-30 20:40:43 +0630

    new file:   readme.md

--- server/matter/index.js
+++ server/matter/index.js
@@ -1,3 +1,27 @@
+
+// This code are copied from https://github.com/WebOrigami/pondlife-zero-deps/blob/main/src/utilities.js
+// by Jan Miksovsky - https://github.com/JanMiksovsky
+
+/**
+ * Parse a string content as a front-matter markdown file.
+ *
+ * The function takes a string content, parse it as a front-matter markdown file,
+ * and returns an object with the following properties:
+ *
+ * - `body`: The content of the markdown file without the front-matter.
+ *
+ * - `key1`, `key2`, ...: The key-value pairs parsed from the front-matter.
+ *
+ * The function uses a regular expression to split the content into two parts:
+ * the front-matter and the body. The front-matter is then parsed as key-value
+ * pairs and added to the data object. The body is the content after the
+ * front-matter.
+ *
+ * @param {string} content The content of the markdown file.
+ *
+ * @returns {Object} An object with the parsed key-value pairs and the body of
+ * the markdown file.
+ */
 export default function matter(content) {
   let text = String(content);
   let data;
